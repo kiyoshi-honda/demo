2004ffc2067e07a3da5e04405a63e290862238c0
kiyoshi-honda <kiyoshi.honda@oit.ac.jp>
2021-11-15 10:59:35 +0900

    demoの追加

---
+++ src/test/java/oit/is/z9999/kaizi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z9999.kaizi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
